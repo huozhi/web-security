606618b9fdebe334b92cd37d54f5d50427fc50ad
lavende <gilesliu12@gmail.com>
2014-11-17 19:46:49 +0800

    delete all code

--- message/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-import datetime
-
-# Create your models here.
-
-class Message(models.Model):
-    date = models.DateField(auto_now=True, auto_now_add=True)
-    content = models.CharField(max_length=200)
